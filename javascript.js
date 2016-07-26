c27aff165eecd3be17c12b5272f3784d017264ee
aboumelhem <anthony.boumelhem1@education.nsw.gov.au>
2016-07-26 11:44:05 +1000

    Delete javascript.js

--- javascript.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    lastElementTop = $('#timeline-events .anitem:last-child').position().top ;
-    scrollAmount = lastElementTop - 200 ;
-    
-$('#listofstuff').animate({scrollTop: scrollAmount},1000);
-});
-
